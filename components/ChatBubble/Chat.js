de02b688ac5bcde4918ee44683f501e04bb73601
FraCanta <you@example.comfcantale14@gmail.com>
2024-03-25 15:50:09 +0100

    chat

--- components/ChatBubble/Chat.js
+++ components/ChatBubble/Chat.js
@@ -1,26 +1,6 @@
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-
-const ChatBubble = ({ message, sender, color, index }) => {
-  const isEven = index % 2 === 0;
-  const bubbleStyles = `bg-${color} text-third font-semibold ${
-    isEven
-      ? "self-start rounded-bl-lg rounded-tr-lg "
-      : "self-end rounded-br-lg rounded-tl-lg"
-  } md:text-xl text-[0.9rem]`;
-
-  return (
-    <motion.div
-      className={`bubble p-3 max-w-[80%] ${bubbleStyles}`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-    >
-      <p className="m-0">{message}</p>
-    </motion.div>
-  );
-};
+import ChatBubble from "./ChatBubble";
 
 const Chat = () => {
   const [messages, setMessages] = useState([
