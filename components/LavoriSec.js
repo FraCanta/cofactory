570081d5ec9a0e962bf3da7420b3cc3ce6df319a
FraCanta <you@example.comfcantale14@gmail.com>
2024-04-09 14:31:55 +0200

    grid works

--- components/LavoriSec.js
+++ components/LavoriSec.js
@@ -1,21 +1,60 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
 const LavoriSec = ({ cards }) => {
-  console.log(cards);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <div className="w-[90%] mx-auto grid grid-cols-1 gap-6 py-8">
       {cards.map((el, i) => {
         return (
-          <Link href={el.button}>
-            <div className="w-full aspect-square md:h-[70vh] relative" key={i}>
+          <Link href={el.button} key={i}>
+            <motion.div
+              className="w-full aspect-square md:h-[70vh] relative"
+              onHoverStart={() => setHoveredIndex(i)}
+              onHoverEnd={() => setHoveredIndex(null)}
+            >
               <Image
                 src={el.img}
                 alt="Milka"
                 fill
-                className="object-cover rounded-lg "
+                className="object-cover rounded-lg"
               />
-            </div>
+
+              <motion.div
+                className="absolute inset-0 bg-third opacity-0 transition-opacity duration-300"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: hoveredIndex === i ? 0.6 : 0 }}
+              ></motion.div>
+              <motion.div
+                className="absolute inset-0 w-full h-full flex items-center justify-center"
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: hoveredIndex === i ? 1 : 0, y: 20 }}
+              >
+                <h2 className="text-white text-3xl lg:text-5xl font-bold w-[90%] mx-auto text-center flex justify-center items-center gap-4">
+                  <span>{el.brand1}</span>{" "}
+                  <motion.span
+                    initial={{ rotate: 0 }}
+                    animate={{ rotate: 45 }}
+                    transition={{
+                      duration: 1,
+
+                      type: "spring",
+                    }}
+                  >
+                    {" "}
+                    <Image
+                      src="/assets/logo/logo_intero.svg"
+                      width={40}
+                      height={10}
+                    />
+                  </motion.span>
+                  <span>{el.brand2}</span>
+                </h2>
+              </motion.div>
+            </motion.div>
           </Link>
         );
       })}
