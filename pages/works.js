aad058544849536823e0e25acabd3dffab3b228a
FraCanta <you@example.comfcantale14@gmail.com>
2024-05-10 16:49:38 +0200

    aquaman

--- pages/works.js
+++ pages/works.js
@@ -11,12 +11,26 @@ import casesEN from "../public/locales/en/cases.json";
 import { GoArrowDown } from "react-icons/go";
 import { MaskText } from "@/components/MaskText";
 import Head from "next/head";
+import BlurryLights from "@/components/layout/BlurryLights";
 const myFont = localFont({ src: "../fonts/ClearfaceStd-Bold.woff" });
 
 const Works = ({ translation }) => {
   // Estrai i casi dal tuo oggetto di traduzione
   const cases = translation.cards;
+  const [showList, setShowList] = useState(false);
+  const [isArrowRotated, setIsArrowRotated] = useState(false); // Stato per la rotazione della freccia
+  const [isTuttiClicked, setIsTuttiClicked] = useState(false); // Stato per il colore del testo "Tutti"
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const toggleList = () => {
+    setShowList(!showList);
+    setIsArrowRotated(!isArrowRotated);
+    setIsTuttiClicked(!isTuttiClicked);
+  };
 
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    toggleList(); // Chiudi il menu a tendina quando viene selezionata una categoria
+  };
   const [hoveredIndex, setHoveredIndex] = useState(null);
   return (
     <>
@@ -29,106 +43,193 @@ const Works = ({ translation }) => {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.6, ease: "easeInOut" }}
       >
+        <div className="absolute top-0 left-0 flex items-center w-full">
+          <BlurryLights />
+        </div>
         <main>
-          <div className="flex justify-between w-[90%] mx-auto mt-40">
+          <div className="flex justify-between w-[90%] mx-auto mt-40 ">
             <div></div>
-            <div className="w-[298px] h-[50px] px-[18px] py-2.5 rounded-[30px] border border-white justify-between items-center inline-flex z-10">
-              <select className="select w-full max-w-xs !bg-transparent">
-                <option value="tutti" defaultValue>
-                  Tutti
-                </option>
-                <option value="brand">Brand</option>
-                <option value="box">Box</option>
-                <option value="movie">Movie</option>
-              </select>
+            <div className="relative" onClick={toggleList}>
+              <div className="flex items-center gap-6">
+                <label
+                  className={`${myFont.className} text-white dark:text-third font-regular lg:text-2xl`}
+                >
+                  Ordina per:
+                </label>
+                <div
+                  className={
+                    showList
+                      ? "min-w-[250px] md:min-w-[300px] h-[50px] px-[18px] py-2.5 rounded-[30px] border text-third bg-white border-white justify-between items-center inline-flex z-[99] font-regular transition-colors duration-300"
+                      : "text-white min-w-[250px] md:min-w-[300px] h-[50px] px-[18px] py-2.5 rounded-[30px] border   border-white justify-between items-center inline-flex transition-colors duration-300"
+                  }
+                >
+                  <button className="  w-full max-w-xs flex items-center justify-between lg:text-2xl ">
+                    {selectedCategory === "All" ? "All" : selectedCategory}
+                    {/* Testo "Tutti" */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 28 28"
+                      className={
+                        isArrowRotated
+                          ? "origin-[50% 55%] rotate-180 transition-all duration-300"
+                          : "transition-all duration-300"
+                      } // Applica la classe per la rotazione
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M4.22 9.47a.75.75 0 0 1 1.06 0L14 18.19l8.72-8.72a.75.75 0 1 1 1.06 1.06l-9.25 9.25a.75.75 0 0 1-1.06 0l-9.25-9.25a.75.75 0 0 1 0-1.06"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {/* Posiziona l'ul in modo assoluto sotto il div con sfondo bianco */}
+                {showList && (
+                  <motion.ul
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }} // Aggiungi la transizione qui
+                    className="absolute top-0 right-0 bg-white min-w-[250px] md:min-w-[300px] lg:text-[25px] z-10 transition-colors duration-300"
+                    style={{
+                      clipPath: "inset(0% round 30px)",
+                      padding: "59px 0 28px",
+                    }}
+                  >
+                    <li
+                      className="hover:bg-pink hover:text-white cursor-pointer"
+                      style={{
+                        padding: "6px 24px",
+                      }}
+                      onClick={() => handleCategorySelect("All")}
+                    >
+                      All
+                    </li>
+                    <li
+                      className="hover:bg-pink hover:text-white cursor-pointer"
+                      style={{
+                        padding: "6px 24px",
+                      }}
+                      onClick={() => handleCategorySelect("Brand")}
+                    >
+                      Brand
+                    </li>
+                    <li
+                      className="hover:bg-pink hover:text-white cursor-pointer"
+                      style={{
+                        padding: "6px 24px",
+                      }}
+                      onClick={() => handleCategorySelect("Box")}
+                    >
+                      Box
+                    </li>
+                    <li
+                      className="hover:bg-pink hover:text-white cursor-pointer"
+                      style={{
+                        padding: "6px 24px",
+                      }}
+                      onClick={() => handleCategorySelect("Movie")}
+                    >
+                      Movie
+                    </li>
+                  </motion.ul>
+                )}
+              </div>
             </div>
           </div>
           <div className="w-[90%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-y-4 lg:gap-4 py-8">
-            {cases.map((el, i) => {
-              const isHovered = hoveredIndex === i;
-              if (i === 2 || i === 5) {
-                return (
-                  <div
-                    key={i}
-                    className="w-full lg:col-span-2"
-                    onMouseEnter={() => setHoveredIndex(i)}
-                    onMouseLeave={() => setHoveredIndex(null)}
-                  >
-                    <Link href={`/cases/${el.button}`}>
-                      <div
-                        className="w-full aspect-square h-[60vh] relative"
-                        data-aos="fade-up"
-                        data-aos-delay="100"
-                      >
-                        <Image
-                          src={el.img}
-                          alt={el.name}
-                          fill
-                          className="object-cover rounded-lg lg:opacity-100 hover:lg:opacity-50 hover:lg:rounded-[250px] hover:transition-all hover:duration-700 opacity-50  hover:lg:grayscale"
-                        />
-                        {isHovered && (
-                          <div className="absolute top-1/2 left-1/2 bottom-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col lg:flex-row items-center justify-center  transition-all duration-1000 ease-in  text-white text-3xl lg:text-4xl font-bold  w-full">
-                            <span className={`${myFont.className}`}>
-                              {el.brand1}
-                            </span>{" "}
-                            <span className="relative h-8 w-8 mx-6">
-                              <Image
-                                src="/assets/logo/per.svg"
-                                fill
-                                className="h-full w-full object-cover"
-                              />
-                            </span>{" "}
-                            <span className={`${myFont.className}`}>
-                              {el.brand2}
-                            </span>
-                          </div>
-                        )}
-                      </div>
-                    </Link>
-                  </div>
-                );
-              } else {
-                return (
-                  <div
-                    key={i}
-                    onMouseEnter={() => setHoveredIndex(i)}
-                    onMouseLeave={() => setHoveredIndex(null)}
-                  >
-                    <Link href={`/cases/${el.button}`}>
-                      <div
-                        className="w-full aspect-square h-[40vh] sm:h-[50vh] lg:h-[60vh] relative"
-                        data-aos="fade-up"
-                        data-aos-delay="100"
-                      >
-                        <Image
-                          src={el.img}
-                          alt={el.name}
-                          fill
-                          className="object-cover rounded-lg lg:opacity-100 hover:lg:opacity-50 hover:lg:rounded-[250px] hover:transition-all hover:duration-700 opacity-50  hover:lg:grayscale"
-                        />
-                        {isHovered && (
-                          <div className="absolute top-1/2 left-1/2 bottom-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col lg:flex-row items-center justify-center  transition-all duration-1000 ease-in  text-white text-3xl lg:text-4xl font-bold  w-full">
-                            <span className={`${myFont.className}`}>
-                              {el.brand1}
-                            </span>{" "}
-                            <span className="relative h-8 w-8 mx-6">
-                              <Image
-                                src="/assets/logo/per.svg"
-                                fill
-                                className="h-full w-full object-cover"
-                              />
-                            </span>{" "}
-                            <span className={`${myFont.className}`}>
-                              {el.brand2}
-                            </span>
-                          </div>
-                        )}
-                      </div>
-                    </Link>
-                  </div>
-                );
-              }
-            })}
+            {cases
+              .filter(
+                (el) =>
+                  selectedCategory === "All" ||
+                  el.categories.some((cat) => cat.name === selectedCategory)
+              )
+              .map((el, i) => {
+                const isHovered = hoveredIndex === i;
+                if (i === 2 || i === 5) {
+                  return (
+                    <div
+                      key={i}
+                      className="w-full lg:col-span-2"
+                      onMouseEnter={() => setHoveredIndex(i)}
+                      onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                      <Link href={`/cases/${el.button}`}>
+                        <div
+                          className="w-full aspect-square h-[60vh] relative"
+                          data-aos="fade-up"
+                          data-aos-delay="100"
+                        >
+                          <Image
+                            src={el.img}
+                            alt={el.name}
+                            fill
+                            className="object-cover rounded-lg lg:opacity-100 hover:lg:opacity-50 hover:lg:rounded-[250px] hover:transition-all hover:duration-700 opacity-50  hover:lg:grayscale"
+                          />
+                          {isHovered && (
+                            <div className="absolute top-1/2 left-1/2 bottom-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col lg:flex-row items-center justify-center  transition-all duration-1000 ease-in  text-white dark:text-third text-3xl lg:text-4xl font-bold  w-full">
+                              <span className={`${myFont.className}`}>
+                                {el.brand1}
+                              </span>{" "}
+                              <span className="relative h-8 w-8 mx-6">
+                                <Image
+                                  src="/assets/logo/per.svg"
+                                  fill
+                                  className="h-full w-full object-cover"
+                                />
+                              </span>{" "}
+                              <span className={`${myFont.className}`}>
+                                {el.brand2}
+                              </span>
+                            </div>
+                          )}
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={i}
+                      onMouseEnter={() => setHoveredIndex(i)}
+                      onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                      <Link href={`/cases/${el.button}`}>
+                        <div
+                          className="w-full aspect-square h-[40vh] sm:h-[50vh] lg:h-[60vh] relative"
+                          data-aos="fade-up"
+                          data-aos-delay="100"
+                        >
+                          <Image
+                            src={el.img}
+                            alt={el.name}
+                            fill
+                            className="object-cover rounded-lg lg:opacity-100 hover:lg:opacity-50 hover:lg:rounded-[250px] hover:transition-all hover:duration-700 opacity-50  hover:lg:grayscale"
+                          />
+                          {isHovered && (
+                            <div className="absolute top-1/2 left-1/2 bottom-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col lg:flex-row items-center justify-center  transition-all duration-1000 ease-in  text-white dark:text-third text-3xl lg:text-4xl font-bold  w-full">
+                              <span className={`${myFont.className}`}>
+                                {el.brand1}
+                              </span>{" "}
+                              <span className="relative h-8 w-8 mx-6">
+                                <Image
+                                  src="/assets/logo/per.svg"
+                                  fill
+                                  className="h-full w-full object-cover"
+                                />
+                              </span>{" "}
+                              <span className={`${myFont.className}`}>
+                                {el.brand2}
+                              </span>
+                            </div>
+                          )}
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                }
+              })}
           </div>
         </main>
       </motion.div>
