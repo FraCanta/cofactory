9dbced9dfca949906ac08abc50a1a4939676f928
FraCanta <you@example.comfcantale14@gmail.com>
2024-06-10 13:49:32 +0200

    tooltip

--- pages/factory.js
+++ pages/factory.js
@@ -6,7 +6,7 @@ import localFont from "next/font/local";
 import { motion } from "framer-motion";
 
 const myFont = localFont({ src: "../fonts/ClearfaceStd-Bold.woff" });
-const myFont2 = localFont({ src: "../fonts/Raleway-Regular.ttf" });
+const myFont2 = localFont({ src: "../fonts/Raleway-light.ttf" });
 import { TbRectangleVerticalFilled } from "react-icons/tb";
 import { MaskText } from "@/components/MaskText";
 import Tippy from "@tippyjs/react";
@@ -25,34 +25,36 @@ const Factory = () => {
       <Hero>
         <MaskText>
           <h1
-            className={`${myFont.className} text-5xl  md:text-[90px]  text-white dark:text-third md:text-center   2xl:text-8xl  `}
+            className={`${myFont.className} text-5xl py-1 md:text-[6rem] text-white dark:text-third lg:text-center 2xl:text-8xl`}
           >
-            Siamo la prima agenzia
-            {/* <Tippy content="*Potrebbero esserci altre agenzie come la nostra ma da accurate ricerche sul web non ci risulta">
-              <button>* </button>
-            </Tippy>{" "} */}
-            <Tippy
-              className="!bg-pink"
-              content={
-                <span className="w-full text-lg !max-w-8xl">
-                  *Potrebbero esserci altre agenzie come la nostra ma da
-                  accurate ricerche sul web non ci risulta
-                </span>
-              }
-            >
-              <button className="text-pink">*</button>
-            </Tippy>{" "}
+            Siamo la prima{" "}
+            <span className="relative mr-4">
+              <span>agenzia</span>
+              <Tippy
+                className="!bg-pink "
+                content={
+                  <span className="w-full text-base !leading-4 font-light">
+                    *Potrebbero esserci altre agenzie come la nostra, ma da
+                    accurate ricerche sul web non ci risulta.
+                  </span>
+                }
+              >
+                <button className="text-pink text-md lg:text-6xl !font-light absolute top-1 lg:top-4   animate-bounce ">
+                  <span className="a">*</span>
+                </button>
+              </Tippy>{" "}
+            </span>
             di{" "}
-            <span className="text-white/60 dark:text-third/60 ">
+            <span className="text-white/60 dark:text-third/60">
               matchmaking
             </span>{" "}
             per brand.
           </h1>
         </MaskText>
-        <div className="lg:w-[65%] mx-auto">
+        <div className="w-full lg:w-[65%] mx-auto">
           <MaskText>
             <p
-              className={`${myFont2.className} font-normal text-[20px] md:text-[30px]  text-white dark:text-third md:text-center  2xl:text-[30px]`}
+              className={`${myFont2.className}  text-[20px] md:text-[25px]  text-white dark:text-third lg:text-center `}
             >
               Ideiamo e realizziamo campagne di comunicazione congiunta.
             </p>
@@ -63,7 +65,7 @@ const Factory = () => {
         <div className="w-[90%] mx-auto">
           <Line />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-[90%] mx-auto ">
+        {/* <div className="grid grid-cols-1 lg:grid-cols-2 w-[90%] mx-auto ">
           <div></div>
           <Paragraph paragraph={paragraph} />
         </div>
@@ -91,9 +93,9 @@ const Factory = () => {
         </div>
         <div className="w-[90%] mx-auto">
           <Line />
-        </div>
+        </div> */}
       </div>
-      <div className="w-[90%] mt-8 mx-auto flex flex-col gap-6 py-14">
+      {/* <div className="w-[90%] mt-8 mx-auto flex flex-col gap-6 py-14">
         <h2
           className={`${myFont.className} text-white dark:text-third uppercase text-[25px]`}
         >
@@ -105,7 +107,7 @@ const Factory = () => {
           <div className="w-[425px] h-[565px] bg-[#D9D9D9]" />
           <div className="w-[425px] h-[565px] bg-[#D9D9D9]" />
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
