b0ca258b7ef2986504c8dff3f90436df1a40ba05
FraCanta <you@example.comfcantale14@gmail.com>
2024-06-18 19:13:16 +0200

    home

--- pages/stories/[title].js
+++ pages/stories/[title].js
@@ -748,7 +748,7 @@ const SingleCases = ({ work, previousWork, nextWork }) => {
             ) : null}
           </div>
         </section>
-        <div className="w-full mx-auto my-10 text-center text-white dark:text-third z-[9999999]">
+        <div className="w-full mx-auto my-10 text-center text-white dark:text-third z-[9999999] overflow-x-hidden">
           {/* <Link href="/works" className={`${myFont.className} text-4xl `}>
             {" "}
             Tutte le storie
