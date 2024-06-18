9e587c2fddde9485328e0c0dd14903b957ec5601
FraCanta <you@example.comfcantale14@gmail.com>
2024-06-18 18:11:47 +0200

    home

--- components/layout/Hero.js
+++ components/layout/Hero.js
@@ -3,7 +3,7 @@ import BlurryLights from "./BlurryLights";
 
 const Hero = forwardRef(({ children }, ref) => {
   return (
-    <div ref={ref} className="relative ">
+    <div ref={ref} className="relative overflow-hidden">
       <div className="absolute w-11/12 mx-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-6 justify-center items-center z-10 py-20">
         {children}
       </div>
