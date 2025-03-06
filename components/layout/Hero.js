db5d5e59f8cb34951efafcc45660cdaec66982d6
FraCanta <fcantale14@gmail.com>
2025-03-06 12:14:04 +0100

    aggiornamenti

--- components/layout/Hero.js
+++ components/layout/Hero.js
@@ -3,7 +3,7 @@ import BlurryLights from "./BlurryLights";
 
 const Hero = ({ children }) => {
   return (
-    <div className="relative h-[90svh] lg:h-[92vh] my-10">
+    <div className="relative min-h-[100svh] lg:h-[92vh] my-10">
       <div className="flex flex-col lg:justify-center items-center absolute top-16 left-0 right-0 bottom-0 lg:inset-0 z-10 w-[90%] mx-auto gap-4">
         {children}
       </div>
